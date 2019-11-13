e9091237a717f92d120ecf3f59997fb1bc479e5f
Tomoya-Sato <tomoya.sato@tier4.jp>
2019-11-13 15:14:46 +0900

    Added voxelgrid

--- ros/src/points_donwsampler/include/points_downsampler/ring_filter.h
+++ ros/src/points_donwsampler/include/points_downsampler/ring_filter.h
@@ -2,11 +2,14 @@
 #define RING_FILTER_H
 
 #include <iostream>
+#include <vector>
+#include <chrono>
 
 #include <sensor_msgs/PointCloud2.h>
 
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 
 #include <velodyne_pointcloud/point_types.h>
 
@@ -25,6 +28,7 @@ class RingFilter
 
     // Params
     int ray_number_;
+    float leafsize_;
 
     // Callback functions
     void pointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
--- ros/src/points_donwsampler/nodes/ring_filter/ring_filter.cpp
+++ ros/src/points_donwsampler/nodes/ring_filter/ring_filter.cpp
@@ -3,6 +3,7 @@
 RingFilter::RingFilter() : nh_(), private_nh_("~")
 {
   private_nh_.param<int>("ray_number", ray_number_, 32);
+  private_nh_.param<float>("leaf_size", leafsize_, 1.5);
 
   filtered_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("filtered_points", 10);
   points_sub_ = nh_.subscribe("points_raw", 1, &RingFilter::pointsCallback, this);
@@ -18,24 +19,45 @@ void RingFilter::pointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
   pcl::PointCloud<pcl::PointXYZI> output;
   pcl::PointCloud<pcl::PointXYZI> tmp;
 
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  start = std::chrono::system_clock::now();
+
   pcl::fromROSMsg(*msg, input);
+
+  std::vector<pcl::PointCloud<pcl::PointXYZI>> cloud_array(ray_number_);
 
   for (pcl::PointCloud<velodyne_pointcloud::PointXYZIR>::const_iterator item = input.begin(); item != input.end(); item++)
   {
-    if (item->ring == 0)
-    {
-      pcl::PointXYZI p;
-      p.x = item->x;
-      p.y = item->y;
-      p.z = item->z;
-      p.intensity = item->intensity;
+    pcl::PointXYZI p;
+    p.x = item->x;
+    p.y = item->y;
+    p.z = item->z;
+    p.intensity = item->intensity;
 
-      tmp.push_back(p);
-    }
+    cloud_array[item->ring].points.push_back(p);
+  }
+
+  pcl::VoxelGrid<pcl::PointXYZI> voxel_grid_filter;
+  voxel_grid_filter.setLeafSize(leafsize_, leafsize_, leafsize_);
+
+  for (int i = 0; i < ray_number_; i++)
+  {
+    pcl::PointCloud<pcl::PointXYZI>::Ptr input_ptr(new pcl::PointCloud<pcl::PointXYZI>(cloud_array[i]));
+    pcl::PointCloud<pcl::PointXYZI>::Ptr filtered_ptr(new pcl::PointCloud<pcl::PointXYZI>());
+
+    voxel_grid_filter.setInputCloud(input_ptr);
+    voxel_grid_filter.filter(*filtered_ptr);
+
+    output += *filtered_ptr;
   }
 
   sensor_msgs::PointCloud2 filtered_msg;
   pcl::toROSMsg(output, filtered_msg);
+
+  end = std::chrono::system_clock::now();
+  double time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0;
+
+  std::cout << "Duration time: " << time << "[ms]" << "\r" << std::flush;
 
   filtered_msg.header = msg->header;
   filtered_pub_.publish(filtered_msg);
